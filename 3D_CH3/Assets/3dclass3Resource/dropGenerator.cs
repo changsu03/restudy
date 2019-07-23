7e24ed11986b6fda3e744ad48907e6ebb2471373
changsu03 <51253789+changsu03@users.noreply.github.com>
2019-07-23 13:24:52 +0900

    한창수
    복습 미완성파일

---
+++ 3D_CH3/Assets/3dclass3Resource/dropGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropGenerator : MonoBehaviour {
+
+    public GameObject prefabglod;
+    public GameObject prefabskull;
+    float sp = 1.0f;
+    float dt = 0;
+    int interval = 2;
+    float Speed = -0.03f;
+
+    public void SetParameter(float sp, float Speed, int interval) {
+        this.sp = sp;
+        this.Speed = Speed;
+        this.interval = interval;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.dt += Time.deltaTime;
+        if(this.dt > this.sp) {
+            this.dt = 0;
+            GameObject item;
+            int dice = 
+        }
+	}
+}
