049ce7f28bfeecc82bc1d7ec20e03cb388ba5d61
changsu03 <51253789+changsu03@users.noreply.github.com>
2019-07-30 16:36:22 +0900

    한창수
    몬스터 이동 및 초 삭제
    거리삭제 및 이동시 태그 변경 필요

---
+++ sam - 복사본/Assets/Leather_Ghost/monster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster : MonoBehaviour
+{
+    int speed = 15;
+    public GameObject 기는몬스터;
+    void Start()
+    {
+
+    }
+    
+    
+    void Update()
+    {
+        float fMove = Time.deltaTime * speed;
+    transform.Translate(Vector3.forward * fMove);
+        Destroy(gameObject, 100.1f);
+    }
+}
